543df2bbc0a29dba86a1bd4fec5f1628c1fcb6ca
David Lebee <david@lebee.info>
2018-12-30 22:09:33 -0600

    added distances

--- src/services/intervals.service.ts
+++ src/services/intervals.service.ts
@@ -4,25 +4,26 @@ export interface IInterval
 {
     intervalSymbol: string;
     name: string;
+    distanceInHalfTones: number;
 }
 
 @Injectable()
 export class IntervalsService
 {  
     static Intervals: Array<IInterval> = [
-        { name: 'tonic', intervalSymbol: '1'},
-        { name: 'minor second', intervalSymbol: 'b2'},
-        { name: 'major second', intervalSymbol: '2'},
-        { name: 'minor third', intervalSymbol: 'b3'},
-        { name: 'major third', intervalSymbol: '3'},
-        { name: 'perfect fourth', intervalSymbol: '4'},
-        { name: 'tritone', intervalSymbol: '4# / b5'},
-        { name: 'perfect fifth', intervalSymbol: '5'},
-        { name: 'minor sixth', intervalSymbol: 'b6'},
-        { name: 'major sixth', intervalSymbol: '6'},
-        { name: 'minor seventh', intervalSymbol: 'b7'},
-        { name: 'major seventh', intervalSymbol: '7'},
-        { name: 'octave', intervalSymbol: '8'},
+        { distanceInHalfTones: 0, name: 'tonic', intervalSymbol: '1'},
+        { distanceInHalfTones: 1, name: 'minor second', intervalSymbol: 'b2'},
+        { distanceInHalfTones: 2, name: 'major second', intervalSymbol: '2'},
+        { distanceInHalfTones: 3, name: 'minor third', intervalSymbol: 'b3'},
+        { distanceInHalfTones: 4, name: 'major third', intervalSymbol: '3'},
+        { distanceInHalfTones: 5, name: 'perfect fourth', intervalSymbol: '4'},
+        { distanceInHalfTones: 6, name: 'tritone', intervalSymbol: '4# / b5'},
+        { distanceInHalfTones: 7, name: 'perfect fifth', intervalSymbol: '5'},
+        { distanceInHalfTones: 8, name: 'minor sixth', intervalSymbol: 'b6'},
+        { distanceInHalfTones: 9, name: 'major sixth', intervalSymbol: '6'},
+        { distanceInHalfTones: 10, name: 'minor seventh', intervalSymbol: 'b7'},
+        { distanceInHalfTones: 11, name: 'major seventh', intervalSymbol: '7'},
+        { distanceInHalfTones: 12, name: 'octave', intervalSymbol: '8'},
     ];
 
     constructor() {
